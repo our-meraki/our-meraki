47298296d03bf71d08484202bbecea2acd0b1ede
our-meraki <89776397+our-meraki@users.noreply.github.com>
2021-08-30 15:24:45 +0530

    Initial

---
+++ source/Service/src/main/java/com/our/meraki/OurMerakiApplication.java
@@ -0,0 +1,15 @@
+package com.our.meraki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurMerakiApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OurMerakiApplication.class, args);
+    }
+
+}
---
+++ source/Service/src/test/java/com/our/meraki/OurMerakiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.our.meraki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurMerakiApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
